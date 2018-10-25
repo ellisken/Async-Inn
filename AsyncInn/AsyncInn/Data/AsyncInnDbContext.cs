a747c80d967a30462c5b5575db74d37d7fd437c5
ellisken <kse104@gmail.com>
2018-10-25 10:32:12 -0700

    define composite keys

--- AsyncInn/AsyncInn/Data/AsyncInnDbContext.cs
+++ AsyncInn/AsyncInn/Data/AsyncInnDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AsyncInn.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +12,22 @@ namespace AsyncInn.Data
         public AsyncInnDbContext(DbContextOptions<AsyncInnDbContext> options) : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HotelRoom>().HasKey(
+                hr => new { hr.HotelID, hr.RoomNumber }
+                );
+
+            modelBuilder.Entity<RoomAmenities>().HasKey(
+                ra => new { ra.AmenitiesID, ra.RoomID }
+                );
+        }
+
+        public DbSet<Amenities> Amenities { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<HotelRoom> HotelRooms { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<RoomAmenities> RoomAmenities { get; set; }
     }
 }
