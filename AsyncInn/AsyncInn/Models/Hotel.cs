da4a4c20a5b324e06ee4958082d3a99d08d66fac
ellisken <kse104@gmail.com>
2018-10-26 09:27:35 -0700

    add annotations to amenities and hotel

--- AsyncInn/AsyncInn/Models/Hotel.cs
+++ AsyncInn/AsyncInn/Models/Hotel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,15 @@ namespace AsyncInn.Models
     public class Hotel
     {
         public int ID { get; set; }
+        [Required(ErrorMessage = "Please enter the hotel's name")]
+        [Display(Name = "Hotel Name")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please provide an address")]
         public string Address { get; set; }
+
+        [Required(ErrorMessage = "Please enter a phone number")]
+        [Display(Name = "Phone Number")]
         public string Phone { get; set; }
 
         //Navigation Properties
--- AsyncInn/AsyncInn/Models/Room.cs
+++ AsyncInn/AsyncInn/Models/Room.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,13 @@ namespace AsyncInn.Models
     public class Room
     {
         public int ID { get; set; }
+
+        [Required(ErrorMessage = "Please enter the room name")]
+        [Display(Name = "Room Name")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage ="Please select a layout")]
+        [EnumDataType(typeof(Layout))]
         public int Layout { get; set; }
 
         //Navigation Properties
