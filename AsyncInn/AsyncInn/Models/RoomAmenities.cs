e0f637c71e9110c16e6496d8e98f8c8386d5dd38
ellisken <kse104@gmail.com>
2018-10-27 18:45:50 -0700

    change dropdown display for RoomAmenities create view

--- AsyncInn/AsyncInn/Models/RoomAmenities.cs
+++ AsyncInn/AsyncInn/Models/RoomAmenities.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,10 +9,12 @@ namespace AsyncInn.Models
 {
     public class RoomAmenities
     {
-        [ForeignKey ("Amenities")]
-        public int AmenitiesID { get; set; }
         [ForeignKey("Room")]
+        [Display(Name = "Room")]
         public int RoomID { get; set; }
+        [ForeignKey ("Amenities")]
+        [Display(Name = "Amenity")]
+        public int AmenitiesID { get; set; }
 
         //Navigation Properties
         public Amenities Amenity { get; set; }
