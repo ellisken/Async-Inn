8a07cadc3849d44f955cba73f368f6c77f15a18d
ellisken <kse104@gmail.com>
2018-10-29 09:39:39 -0700

    start service and interface for amenities

---
+++ AsyncInn/AsyncInn/Models/Services/AmenitiesService.cs
@@ -0,0 +1,19 @@
+﻿using AsyncInn.Data;
+using AsyncInn.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Models.Services
+{
+    public class AmenitiesService : IAmenities
+    {
+        private AsyncInnDbContext _context;
+
+        public AmenitiesService(AsyncInnDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
