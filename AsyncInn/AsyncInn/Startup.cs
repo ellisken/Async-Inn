6c4a391c18717f1044e2453f92b0e2eafdf4e138
ellisken <kse104@gmail.com>
2018-11-02 15:02:38 -0700

    update layout

--- AsyncInn/AsyncInn/Startup.cs
+++ AsyncInn/AsyncInn/Startup.cs
@@ -18,9 +18,11 @@ namespace AsyncInn
     {
         public IConfiguration Configuration { get; }
 
-        public Startup(IConfiguration configuration)
+        public Startup()
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder().AddEnvironmentVariables();
+            builder.AddUserSecrets<Startup>();
+            Configuration = builder.Build();
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -30,7 +32,7 @@ namespace AsyncInn
             services.AddMvc();
 
             services.AddDbContext<AsyncInnDbContext>(options =>
-            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            options.UseSqlServer(Configuration.GetConnectionString("ProductionKey")));
 
             services.AddTransient<IAmenities, AmenitiesService>();
             services.AddTransient<IHotel, HotelService>();
